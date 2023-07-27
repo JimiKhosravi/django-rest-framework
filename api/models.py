ea6a15193a7281f6b7a68de7989f61b5d8f2dec8
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2023-07-27 11:47:54 +0200

    Create model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models):
+    name = models.CharField(max_length=300)
+    age = models.PositiveIntegerField()
+    bio = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
